afc6c1605807140502820c634fcd8162dd27d801
Hob Spillane <hob.spillane@workday.com>
2017-11-10 15:54:22 -0500

    Moved server code to a sub-folder

---
+++ lambdas/publish.sh
@@ -0,0 +1,6 @@
+zip -r app.zip . -x \*client* \*.git*
+aws lambda update-function-code --region us-east-1 --function-name getReadings --zip-file fileb:///Users/hob.spillane/code/thermolambda/lambdas/app.zip
+aws lambda update-function-code --region us-east-1 --function-name getLatestReading --zip-file fileb:///Users/hob.spillane/code/thermolambda/lambdas/app.zip
+aws lambda update-function-code --region us-east-1 --function-name cleanFutureRows --zip-file fileb:///Users/hob.spillane/code/thermolambda/lambdas/app.zip
+aws lambda update-function-code --region us-east-1 --function-name getSampleReadings --zip-file fileb:///Users/hob.spillane/code/thermolambda/lambdas/app.zip
+rm -f app.zip
--- publish.sh
+++
@@ -1,6 +0,0 @@
-zip -r app.zip . -x \*client* \*.git*
-aws lambda update-function-code --region us-east-1 --function-name getReadings --zip-file fileb:///Users/hob.spillane/code/thermolambda/app.zip
-aws lambda update-function-code --region us-east-1 --function-name getLatestReading --zip-file fileb:///Users/hob.spillane/code/thermolambda/app.zip
-aws lambda update-function-code --region us-east-1 --function-name cleanFutureRows --zip-file fileb:///Users/hob.spillane/code/thermolambda/app.zip
-aws lambda update-function-code --region us-east-1 --function-name getSampleReadings --zip-file fileb:///Users/hob.spillane/code/thermolambda/app.zip
-rm -f app.zip
